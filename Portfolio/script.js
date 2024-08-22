c1cb5a8a51f20afa4bad5e6b075e6821228d88da
Danyelle Melo <danyelle.dados@gmail.com>
2024-08-22 09:39:39 -0300

    up

---
+++ Portfolio/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
