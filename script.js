152539a60555cf973ffab3ee1c30214fa90dff14
Danyelle Melo <danyelle.dados@gmail.com>
2024-08-28 14:04:32 -0300

    update all

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
-            targetElement.scrollIntoView({
-                behavior: 'smooth'
-            });
+// Selecionar os elementos
+const spans = document.querySelectorAll('.computador span'); //Usamos querySelectorAll para selecionar todos os elementos com a classe computador span e janelinha.
+const janelinhas = document.querySelectorAll('.janelinha');
+
+// Adicionar evento de clique a cada span
+spans.forEach((span, index) => { // O método forEach itera sobre cada span e adiciona um evento de clique.
+    span.addEventListener('click', () => {
+        // Ocultar todas as janelinhas
+        janelinhas.forEach(janelinha => {
+            janelinha.style.display = 'none';
         });
+
+        // Mostrar a janelinha correspondente
+        janelinhas[index].style.display = 'block';
+    });
+});
+
+spans.forEach((span, index) => {
+    // ...
+    janelinhas[index].querySelector('.fechar').addEventListener('click', () => {
+        janelinhas[index].style.display = 'none';
+    });
+
+    janelinhas[index].querySelector('.fechar').addEventListener('click', () => {
+        janelinhas[index].classList.add('fechar');
     });
 });
